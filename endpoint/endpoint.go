c9b8fdf8c5b7b5f435b2f04f35eb8a10a926eee2
王道林 <332132393@qq.com>
2019-12-27 16:38:31 +0800

    modify for go mod

--- endpoint/endpoint.go
+++ endpoint/endpoint.go
@@ -6,11 +6,11 @@ import (
 
 // Endpoint is the fundamental building block of servers and clients.
 // It represents a single RPC method.
-type Endpoint func(ctx context.Context, request interface{}) (response interface{}, err error)
+type Endpoint func(ctx context.Context, method, rawUrl string, headers map[string]string, reqObj interface{}, respObj interface{}) (resp interface{}, err error)
 
 // Nop is an endpoint that does nothing and returns a nil error.
 // Useful for tests.
-func Nop(context.Context, interface{}) (interface{}, error) { return struct{}{}, nil }
+func Nop(context.Context, interface{}, map[string]string, string) (interface{}, error) { return struct{}{}, nil }
 
 // Middleware is a chainable behavior modifier for endpoints.
 type Middleware func(Endpoint) Endpoint
